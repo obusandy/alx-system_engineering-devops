6e8942c68e0fb6f0de33cab1ac38d5aa9264f8ac
obusandy <obungasandrah@gmail.com>
2024-03-26 06:45:35 -0700

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -15,11 +15,11 @@ if __name__ == "__main__":
 
     sessionReq = requests.Session()
 
-    idEmp = argv[1]
-    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
-    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
+    EmpyeeID = argv[1]
+    URLidentty = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(EmpyeeID)
+    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(EmpyeeID)
 
-    employee = sessionReq.get(idURL)
+    employee = sessionReq.get(URLidentty)
     employeeName = sessionReq.get(nameURL)
 
     json_req = employee.json()
@@ -27,13 +27,13 @@ if __name__ == "__main__":
 
     todos = 0
 
-    for done_tasks in json_req:
-        if done_tasks['completed']:
+    for dontasks in json_req:
+        if dontasks['completed']:
             todos += 1
 
-    fileCSV = idEmp + '.csv'
+    fileCSV = EmpyeeID + '.csv'
 
     with open(fileCSV, "w", newline='') as csvfile:
         write = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
         for intg in json_req:
-            write.writerow([idEmp, usser, intg.get('completed'), intg.get('title')])
+            write.writerow([EmpyeeID, usser, intg.get('completed'), intg.get('title')])
