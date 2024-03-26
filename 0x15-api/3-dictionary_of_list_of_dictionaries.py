013d51698dbd0f53a50a357d9f97facc441c7605
obusandy <sandraobunga29@gmail.com>
2024-03-26 16:11:31 +0300

    executable

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 This is a scrit that reacords all tasks from all employees
-It extends the 
+It extends the  Python script to export data in the JSON format.
 """
 
 import json
@@ -20,6 +20,8 @@ if __name__ == "__main__":
     tasktodos = requests.get('https://jsonplaceholder.typicode.com/todos')
     tasktodos = tasktodos.json()
     todolist = {}
+    
+#list
 
     for usr in users:
         taskList = []
