6f8f4c20b87f09a5314fd6d3515f851142fc9998
obusandy <obungasandrah@gmail.com>
2024-04-12 09:50:41 -0700

    Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+# contains Puppet code
+# strace can attach to a current running process
+
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
