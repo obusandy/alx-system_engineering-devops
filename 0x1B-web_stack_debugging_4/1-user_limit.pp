54c939460bbd3181394ea5e2cb800233625d3bac
obusandy <obungasandrah@gmail.com>
2024-04-18 04:01:37 -0700

    my changes to user limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,15 @@
+# OS configuration so that it is possible to login with the holberton user
+# Increase open file limit for the 'holberton' user to accommodate
+
+exec {'replace-1':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before   => Exec['replace-2'],
+}
+
+# Adjust open file limit for general system processes 
+
+exec {'replace-2':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+}
